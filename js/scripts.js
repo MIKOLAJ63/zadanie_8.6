59959a6b1ad74e57474cf3a7bf4e683ee7128b83
MIKOLAJ63 <MIKOLAJ63>
2017-08-15 12:29:33 +0200

    Conditional statement

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+//scripts.js
+var a=4;
+var b=6;
+var value=(a*a)+(2*a*b)-(b*b);
+console.log('Action result:'+value);
+if (value>0) {
+  console.log('Result positive');}
+else if(value<0) {
+  console.log('Result negative');}
+else {
+  console.log('Result equal to 0');}
+
+//scripts.js
+var a=prompt('Insert value a');
+var b=prompt('Insert value b');
+var value=(a*a)+(2*a*b)-(b*b);
+console.log('Action result:'+value);
+if (value>0) {
+  console.log('Result positive');}
+else if(value<0) {
+  console.log('Result negative');}
+else {
+  console.log('Result equal to 0');}
